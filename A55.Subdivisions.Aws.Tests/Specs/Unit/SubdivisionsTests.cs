40a03ad2611cb63caf83d272ca8e04254ab0a859
Lucas Teles <lucas.teles@outlook.com>
2022-10-14 13:00:44 -0300

    WIP

---
+++ A55.Subdivisions.Aws.Tests/Specs/Unit/SubdivisionsTests.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.Runtime;
+using Docker.DotNet;
+
+namespace A55.Subdivisions.Aws.Tests.Specs.Unit;
+
+public class SubdivisionsTests
+{
+    [Test]
+    public void ShouldInstantiateClient()
+    {
+        var client = Subdivisions.CreateClient(c =>
+        {
+        }, new AnonymousAWSCredentials());
+
+        client.Should().NotBeNull();
+    }
+
+    [Test]
+    public void ShouldInstantiateConsumer()
+    {
+        var client = Subdivisions.CreateConsumer(c =>
+        {
+        }, new AnonymousAWSCredentials());
+
+        client.Should().NotBeNull();
+    }
+
+    [Test]
+    public void ShouldInstantiateProducer()
+    {
+        var client = Subdivisions.CreateProducer(c =>
+        {
+        }, new AnonymousAWSCredentials());
+
+        client.Should().NotBeNull();
+    }
+}
---
+++ A55.Subdivisions.Aws/Subdivisions.cs
@@ -0,0 +1,33 @@
+﻿using A55.Subdivisions.Models;
+using Amazon.Runtime;
+using Amazon.Util;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace A55.Subdivisions;
+
+public static class Subdivisions
+{
+    public static ISubdivisionsClient CreateClient(
+        Action<SubConfig>? config = null,
+        AWSCredentials? credentials = null,
+        Action<ILoggingBuilder>? logConfig = null
+    ) =>
+        new ServiceCollection()
+            .AddSubdivisionsServices(config, credentials)
+            .AddLogging(logConfig ?? delegate { })
+            .BuildServiceProvider()
+            .GetRequiredService<ISubdivisionsClient>();
+
+    public static IProducerClient CreateProducer(
+        Action<SubConfig>? config = null,
+        AWSCredentials? credentials = null,
+        Action<ILoggingBuilder>? logConfig = null
+    ) => CreateClient(config, credentials, logConfig);
+
+    public static IConsumerClient CreateConsumer(
+        Action<SubConfig>? config = null,
+        AWSCredentials? credentials = null,
+        Action<ILoggingBuilder>? logConfig = null
+    ) => CreateClient(config, credentials, logConfig);
+}
